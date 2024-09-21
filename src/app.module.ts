61f27e27fac2354c04a577081ebc3d7b2e72f9f0
Asad Siddique <asadsiddique.a@gmail.com>
2024-09-21 09:13:28 +0500

    feat: learn how to log

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,26 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SongsModule } from './songs/songs.module';
+import { LoggerMiddleware } from './common/middleware/logger/logger.middleware';
 
 @Module({
   imports: [SongsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    // for all methods / Request on songs path...
+    // consumer.apply(LoggerMiddleware).forRoutes('songs');
+
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes({ path: 'songs', method: RequestMethod.GET });
+  }
+}
